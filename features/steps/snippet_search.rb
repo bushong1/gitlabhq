7e193e5b29b3ab6515ff547c49f6e2bd08065848
Charles Bushong <bushong1@gmail.com>
2014-09-05 13:09:50 -0400

    Adding tests for searching snippets

--- features/steps/snippet_search.rb
+++ features/steps/snippet_search.rb
@@ -2,11 +2,39 @@ class Spinach::Features::SnippetSearch < Spinach::FeatureSteps
   include SharedAuthentication
   include SharedPaths
   include SharedSnippet
-  include UserSnippets
+  include SharedUser
+  include SharedSearch
 
-  step 'I search for "snippet"' do
-    fill_in "search", with: "snippet"
-    click_button :submit
+  step 'I search for "snippet" in snippet titles' do
+    search_snippet_titles "snippet"
+  end
+
+  step 'I search for "snippet private" in snippet titles' do
+    search_snippet_titles "snippet private"
+  end
+
+  step 'I search for "line seven" in snippet contents' do
+    search_snippet_contents "line seven"
+  end
+
+  step 'I should see "line seven" in results' do
+    page.should have_content "line seven"
+  end
+
+  step 'I should see "line four" in results' do
+    page.should have_content "line four"
+  end
+
+  step 'I should see "line ten" in results' do
+    page.should have_content "line ten"
+  end
+
+  step 'I should not see "line eleven" in results' do
+    page.should_not have_content "line eleven"
+  end
+
+  step 'I should not see "line three" in results' do
+    page.should_not have_content "line three"
   end
 
   Then 'I should see "Personal snippet one" in results' do
